d0ce8a68bfdc92e4af3b1529a6665482c6ee2428
Sourav <souravsahu70047@gmail.com>
2023-04-18 20:33:57 +0530

    Added Leaderboard Page

--- number-game-app/src/Pages/LeaderBoard.jsx
+++ number-game-app/src/Pages/LeaderBoard.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import {Container,Box,Heading,Text,Button} from "@chakra-ui/react"
+import {useEffect} from "react";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 const LeaderBoard = () => {
+    const [data,setdata]=useState([]);
+
+    const getData=()=>{
+        axios.get("https://mock-server-app-2-3le7.onrender.com/scores")
+        .then((res)=>{
+            // console.log(res)
+            setdata(res.data)
+        })
+        .catch((err)=>console.log(err))
+    }
+    useEffect(() => {
+        getData()
+    }, []);
   return (
-    <div>LeaderBoard</div>
+    <Container bg="#9DC4FB" maxW="full" h="1500px" mt={0} centerContent overflow="hidden">
+    <Box   bg="#02054B"
+    w="60%"
+          color="white"
+          borderRadius="lg"
+          m={{ sm: 4, md: 16, lg: 10 }}
+          p={{ sm: 5, md: 5, lg: 16 }} >
+        <Heading size="lg" as="h3" fontWeight="400" p="1%">Leaderboard</Heading>  
+            <Box p="4">
+               {data&&data.map((el)=>(
+                <div key={el.id} style={{display:"flex",justifyContent:"space-around",fontSize:"20px"}} >
+                    <p><b>{el.name}</b></p>
+                    <p>{el.difficulty}</p>
+                    <p>{el.score}</p>
+                </div>
+               ))}
+            </Box>
+            <Link to="/" ><Button colorScheme="blue" p="3%" m="3%" >Start a New Game</Button></Link>
+
+    </Box>
+   </Container>
   )
 }
 
--- number-game-app/src/index.js
+++ number-game-app/src/index.js
@@ -6,14 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import { store } from './Redux/store';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store} >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <ChakraProvider>
+    <Provider store={store} >    
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
